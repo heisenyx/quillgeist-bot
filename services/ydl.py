53fc41d92c37f332fc5fcaa2864baf27cec460fe
heisen <mclegendnew@gmail.com>
2025-09-11 18:55:23 +0200

    fix: add cookies support

--- services/ydl.py
+++ services/ydl.py
@@ -1,16 +1,36 @@
+import os
+import tempfile
+
 import yt_dlp
 from telegram import InputMediaVideo
+
+from config import YDL_COOKIES
 from utils.logger import setup_logger
 
 logger = setup_logger()
 
-ydl_opts = {
-    'noplaylist': True,
-    'quiet': True,
-}
+def get_ydl_opts():
+    opts = {
+        'noplaylist': True,
+        'quiet': True,
+    }
+
+    if YDL_COOKIES:
+        temp_cookie_file = tempfile.NamedTemporaryFile(mode='w+', delete=False, encoding='utf-8')
+        temp_cookie_file.write(YDL_COOKIES)
+        temp_cookie_file.close()
+
+        opts['cookiefile'] = temp_cookie_file.name
+        logger.info("Using cookies from env variable")
+        return opts, temp_cookie_file.name
+
+    logger.warning("YDL_COOKIES environment variable not set. Proceeding without authentication")
+    return opts, None
 
 async def process(url: str) -> list[InputMediaVideo] | None:
     logger.info(f"Extracting info for URL: {url}")
+
+    ydl_opts, temp_cookie_path = get_ydl_opts()
     direct_url = None
 
     try:
@@ -30,5 +50,9 @@ async def process(url: str) -> list[InputMediaVideo] | None:
                 return None
 
     except Exception as e:
-        logger.error(f"Failed to process Instagram URL {url}: {e}")
+        logger.error(f"Failed to process URL {url}: {e}")
         return None
+    finally:
+        if temp_cookie_path and os.path.exists(temp_cookie_path):
+            os.remove(temp_cookie_path)
+            logger.info(f"Cleaned up temporary cookie file: {temp_cookie_path}")
